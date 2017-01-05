eaa1a74bdd1a315980ca38404dc87538f7524c75
David <Davidteri91@gmail.com>
2017-01-05 14:28:01 +0100

    sony-common: include sony sepolicy
    make it not mandatory for now ... until it is ready
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -83,4 +83,5 @@ BUILD_KERNEL := true
 -include vendor/qcom/proprietary/common/build/proprietary-build.mk
 
 # SELinux
-include device/sony/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
+-include device/sony/sepolicy/sepolicy.mk
